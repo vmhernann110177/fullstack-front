bf0c9267753e3d524fbfa8c484e1fc3bf819f5e0
Victor Hernández <65037576+vmhernann110177@users.noreply.github.com>
2021-12-23 14:19:56 -0600

    Cambios necesarios para Disponibilidad

--- js/formulario.js
+++ js/formulario.js
@@ -25,6 +25,30 @@ function cargarP1() {
 
 }
 
+intContador=0;
+function Disponibilidad() {
+        //// OJO Aqui le estoy colocando la funcionalidad al label Disponibilidad
+        //// Aquí Pruebo que funciona con el label alertas:.. 
+        // $("#alertas").text("¡El campo de tus apellidos esta vacio, colócalo por favor!");
+        // Si funciona entonces lo deshabilito
+        // Si cambia al lblDisponibilidad!!
+        //Creo un contador para marcar en consola numero de intentos
+        intContador = intContador +1;
+        // $("#lblDisponibilidad").text("NO DISPONIBLE..");
+        // Obten el valor cambiado del combobox y haz un if para decidir
+        valor = document.getElementById("p6").value
+
+        if (valor =="Pequeña") {
+            $("#lblDisponibilidad").text("NO DISPONIBLE.");
+            console.log("Prueba con consola, " + intContador + ", NO DISPONIBLE");
+        }
+        else
+        {
+            $("#lblDisponibilidad").text("DISPONIBLE.");
+            console.log("Prueba con consola, " + intContador + ", DISPONIBLE");
+        }
+}
+
 // function showAlert(mensaje) {
 //     document.getElementById("alertas").innerHTML = mensaje
 // }
